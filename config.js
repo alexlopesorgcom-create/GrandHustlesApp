e2a7f82ec313178a4b2f2a52cbf8f6d3984daf84
alexlopesorgcom-create <alexlopes.org.com@gmail.com>
2026-07-29 20:37:21 -0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Si es un archivo de configuración JS:
-const STRIPE_KEYS = {
-  publicKey: "pk_live_51TybDhLwe6dh6ocORfqtZjlzLrmr6o6p5ek7YyaAUSXTbI4VSpQHrawrGqBCDXlhNXVzgCtQ8bfL1mnFEWQNSHmh0023zd9CtW"
-};
